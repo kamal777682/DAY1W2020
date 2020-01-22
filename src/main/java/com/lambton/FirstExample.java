23fda6c952e46cc96af300d7e28745fa28a667f0
MacStudent <macstudent@128-IMAC19.local>
2020-01-21 20:48:45 -0500

    [Kamal] Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,15 @@
+package com.lambton;
+
+public class FirstExample
+
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" Welcome to Java programming");
+        for (int i = 1; i <=10 ; i++)
+        {
+            System.out.println("kamalpreet kaur");
+        }
+    }
+}
+ 
