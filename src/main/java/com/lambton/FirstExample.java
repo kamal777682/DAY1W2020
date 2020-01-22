f8099fc3b9df0e16d7cef7c8f352149348799ecf
macstudent <Kamal777682>
2020-01-22 18:46:41 -0500

    day 2  - data types,type casting

--- src/main/java/com/lambton/FirstExample.java
+++ src/main/java/com/lambton/FirstExample.java
@@ -11,6 +11,36 @@ public class FirstExample
             System.out.println("kamalpreet kaur");
             // Sample Commit -1
             System.out.println(" first Git hub program");
+            int a = 100;
+            int b;
+            b=200;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println("Sum = "+(a+b));
+            System.out.println(a+b+"Sum");
+            short x= 32767;
+            System.out.println("Short Max=" + Short.MAX_VALUE);
+            System.out.println("Short Min =" +Short.MIN_VALUE);
+
+            long y = 214783647;
+            System.out.println(y);
+            float f1 = 1000.50F;
+            double f2= 1000.40;
+            System.out.println("F1 = "+f1);
+            System.out.println("F2 = "+f2);
+            float c=10/3f;
+
+            System.out.println(c);
+            float c1 = 10f/3f;
+            System.out.println(c1);
+            int c2 = (int)(10f/3);
+            System.out.println(c2);
+
+            float c3 = (float)10/3;
+            System.out.println(c3);
+
+            double x1 = 1_00_00.52 / 10;
+            System.out.println(x1);
         }
     }
 }
